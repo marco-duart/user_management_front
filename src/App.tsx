b03c15a49965d02ce5f96c727274afc617e84ea8
marco-duart <marcoaurelio_db@hotmail.com>
2024-12-28 19:50:11 -0300

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./assets/styles/global-styles";
+import Router from "./routes";
+import { light } from "./assets/styles/theme";
 
 function App() {
   return (
     <>
+      <ThemeProvider theme={light}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
